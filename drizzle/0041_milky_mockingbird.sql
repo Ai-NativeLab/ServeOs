0dc92e91c814152328f2bd64caac28bb4d637b99
Mohaned Sayed <29494033+mohanedsayed@users.noreply.github.com>
2026-08-14 02:51:40 +0300

    chore: remove branch migrations before merge

--- drizzle/0041_milky_mockingbird.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "pos_sync_event_receipts" ADD COLUMN "seq" integer;--> statement-breakpoint
-CREATE INDEX "pos_sync_event_receipts_device_seq" ON "pos_sync_event_receipts" USING btree ("device_id","seq");
